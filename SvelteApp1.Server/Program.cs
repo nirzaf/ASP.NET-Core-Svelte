a3d2181db11bc5a092cdb294946e96423bfabb95
nirzaf <mfmfazrin1986@gmail.com>
2024-01-06 14:57:38 +0300

    Refactor code and update package versions
    Removed unnecessary comments and spaces in the Program.cs file to improve readability and maintain clean code. In the *.csproj file, updated the version for Microsoft.AspNetCore.SpaProxy package from '8.*-*' to '8.0.0' and Swashbuckle.AspNetCore package from '6.4.0' to '6.5.0' to keep packages up to date.

--- SvelteApp1.Server/Program.cs
+++ SvelteApp1.Server/Program.cs
@@ -13,24 +13,18 @@ public class Program
         var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
         builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-
         builder.Services.AddAuthorization();
         builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
             .AddEntityFrameworkStores<ApplicationDbContext>();
 
-        // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-
         var app = builder.Build();
 
         app.UseDefaultFiles();
         app.UseStaticFiles();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -38,33 +32,23 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
         app.MapIdentityApi<ApplicationUser>();
 
         app.MapPost("/logout", async (SignInManager<ApplicationUser> signInManager) =>
         {
-
             await signInManager.SignOutAsync();
             return Results.Ok();
-
         }).RequireAuthorization();
-
-
+        
         app.MapGet("/pingauth", (ClaimsPrincipal user) =>
         {
             var email = user.FindFirstValue(ClaimTypes.Email); // get the user's email from the claim
             return Results.Json(new { Email = email }); ; // return the email as a plain text response
         }).RequireAuthorization();
 
-
-
-
         app.MapControllers();
-
         app.MapFallbackToFile("/index.html");
-
         app.Run();
     }
 }
